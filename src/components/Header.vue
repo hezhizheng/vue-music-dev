c27384afd278435bec26e28f419d9871beeb94f8
hezhizheng <dexter.ho.cn@gmail.com>
2020-03-09 13:46:21 +0800

    fix: replace https logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="musicHeader">
-    <mt-header fixed title="@www.hzz.cool" >
 
+
+    <mt-header fixed title="@www.hzz.cool" to="//github.com"
+               is-link>
       <router-link to="/" slot="left">
-        <img class="logo" src="http://m.kugou.com/v3/static/images/index/logo.png">
+        <img class="logo" src="https://i.loli.net/2020/03/09/E2KnBV9ag6cZqdU.png">
       </router-link>
 
       <router-link to="/search" slot="right">
